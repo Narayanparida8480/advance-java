2a317222c8297b84fc9902b79c933ba363eaed56
Narayan parida <pikuluprince@gmail.com>
2026-08-20 01:04:58 +0530

    Add files via upload

---
+++ jdbc/src/main/java/org/example/entity/student.java
@@ -0,0 +1,13 @@
+package org.example.entity;
+
+public class student {
+    private int id;
+    private String name;
+    private String domain;
+    student(int id,String name,String domain){
+        this.id=id;
+        this.name=name;
+        this.domain=domain;
+
+    }
+}
